f324eb9670927523f1b9b10bfaf0dc1afd8bfc0a
Dwarslooper <dwarslooper2910@gmx.de>
2024-10-06 19:57:36 +0200

    Initial commit

---
+++ src/main/java/com/dwarslooper/extent/simpleserverswitcher/Main.java
@@ -0,0 +1,95 @@
+package com.dwarslooper.extent.simpleserverswitcher;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.api.plugin.PluginManager;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Main extends Plugin {
+
+    private static Main instance;
+
+    private Configuration config;
+    private final List<ProvidedServer> servers = new ArrayList<>();
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        PluginManager pm = getProxy().getPluginManager();
+
+        pm.registerCommand(this, new ReloadCommand());
+
+        instance = this;
+
+        reload(pm);
+
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public void reload(PluginManager pm) {
+        try {
+            servers.forEach(providedServer -> pm.unregisterCommand(providedServer.getCommand()));
+            servers.clear();
+
+            File configFile = new File(getInstance().getDataFolder(), "config.yml");
+
+            if(!configFile.exists()) {
+                configFile.getParentFile().mkdirs();
+                try(InputStream stream = getResourceAsStream("config.yml"); OutputStream fileOutputStream = new FileOutputStream(configFile)) {
+                    fileOutputStream.write(stream.readAllBytes());
+                }
+            }
+
+            config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
+
+            Configuration serversConfig = config.getSection("servers");
+
+            serversConfig.getKeys().forEach(s -> {
+                Configuration server = serversConfig.getSection(s);
+                servers.add(new ProvidedServer.Parser(server).buildOrThrow(s));
+            });
+
+            servers.forEach(providedServer -> pm.registerCommand(this, providedServer.getCommand()));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public Configuration getConfig() {
+        return config;
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    private static class ReloadCommand extends Command {
+
+        public ReloadCommand() {
+            super("ssreload", "simpleserverswitcher.reload");
+        }
+
+        @Override
+        public void execute(CommandSender commandSender, String[] strings) {
+            getInstance().reload(getInstance().getProxy().getPluginManager());
+            commandSender.sendMessage(new ComponentBuilder("§aReloaded (%s servers listed)".formatted(getInstance().servers.size())).create());
+        }
+    }
+
+}
---
+++ src/main/java/com/dwarslooper/extent/simpleserverswitcher/ProvidedServer.java
@@ -0,0 +1,83 @@
+package com.dwarslooper.extent.simpleserverswitcher;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.config.Configuration;
+
+import javax.annotation.Nullable;
+
+public class ProvidedServer {
+
+    private final String id;
+    private final String permission;
+    private final boolean restricted;
+    private final Command command;
+
+    public ProvidedServer(String id, String command, @Nullable String permission, boolean restricted) {
+        this.id = id;
+        this.permission = permission;
+        this.restricted = restricted;
+        this.command = new Command(command);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Command getCommand() {
+        return command;
+    }
+
+    public String getCommandName() {
+        return command.getName();
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public boolean isRestricted() {
+        return restricted;
+    }
+
+    public class Command extends net.md_5.bungee.api.plugin.Command {
+
+        public Command(String name) {
+            super(name, ProvidedServer.this.permission);
+            setPermissionMessage("§cYou are lacking the permission node §f%s §crequired to connect to this server.".formatted(getPermission()));
+        }
+
+        @Override
+        public void execute(CommandSender commandSender, String[] args) {
+            accept(commandSender);
+        }
+
+        public void accept(CommandSender commandSender) {
+            if(commandSender instanceof ProxiedPlayer player) {
+                ServerInfo info = Main.getInstance().getProxy().getServerInfo(getId());
+                if(info == null) commandSender.sendMessage(new ComponentBuilder("§cThe target server is not registered on the proxy. Please report this to an admin.").create());
+                else player.connect(info);
+            } else {
+                commandSender.sendMessage(new ComponentBuilder("§cThis command can only be ran by players").create());
+            }
+        }
+
+    }
+
+    public static class Parser {
+
+        private final Configuration configuration;
+
+        public Parser(Configuration configuration) {
+            this.configuration = configuration;
+        }
+
+        public ProvidedServer buildOrThrow(String id) {
+            boolean restricted = configuration.getBoolean("restricted");
+            return new ProvidedServer(id, configuration.getString("command"), restricted ? configuration.getString("permission") : null, restricted);
+        }
+    }
+
+}
